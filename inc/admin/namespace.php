19b5eb7c1b88aaede3a3467f8929eb5e8ac47958
Robert O'Rourke <robert@o-rourke.org>
2018-08-08 18:26:53 +0100

    remove re-process button, could get expensive

--- inc/admin/namespace.php
+++ inc/admin/namespace.php
@@ -6,12 +6,7 @@ use HM\AWS_Rekognition;
 use WP_Post;
 
 function bootstrap() {
-	if ( ! current_user_can( 'update_rekognition_labels' ) ) {
-		return;
-	}
-
 	add_meta_box( 'hm_aws_rekognition_image_labels', __( 'Detected Image Labels', 'hm-aws-rekognition' ), __NAMESPACE__ . '\\output_metabox', 'attachment', 'side' );
-	add_action( 'wp_ajax_hm-aws-rekognition-update-labels', __NAMESPACE__ . '\\admin_ajax_update_labels' );
 }
 
 function output_metabox( WP_Post $post ) {
@@ -25,24 +20,5 @@ function output_metabox( WP_Post $post ) {
 
 	?>
 	<pre style="white-space: pre-wrap" id="hm-aws-rekognition-labels"><?php echo esc_html( implode( ', ', $labels ) ) ?></pre>
-	<button class="button button-secondary hm-aws-rekognition-update-labels"><?php _e( 'Re-process', 'hm-aws-rekognition' ) ?></button>
 	<?php
-}
-
-function admin_ajax_update_labels() {
-	error_reporting( E_ALL );
-	ini_set( 'display_errors', 'on' );
-
-	$post_id = intval( $_GET['id'] );
-	$update = AWS_Rekognition\update_attachment_data( $post_id );
-
-	if ( is_wp_error( $update ) ) {
-		wp_send_json_error( $update );
-		exit;
-	}
-
-	$labels = AWS_Rekognition\get_attachment_labels( $post_id );
-
-	echo wp_json_encode( $labels );
-	exit;
 }
